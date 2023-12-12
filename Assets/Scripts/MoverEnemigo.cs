2d7fcc481a79f8a14ca6d3b3e2cc194fb5421a8d
GreysyBurgos <SesmaWTF@gmail.com>
2023-12-12 14:45:40 +0100

    Enemigo y comienzo de escena Game Over

---
+++ Assets/Scripts/MoverEnemigo.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverEnemigo : MonoBehaviour
+{
+    [SerializeField] Camera camara;
+    [SerializeField] Vector2 posicionInicial, posicionMinima;
+    [SerializeField] float velocidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camara = Camera.main;
+        posicionMinima = camara.ViewportToWorldPoint(new Vector2(0, 0));
+        posicionInicial = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * velocidad * Time.deltaTime);
+        if (transform.position.x < posicionMinima.x)
+        {
+            transform.position = posicionInicial;
+            velocidad++;
+        }
+    }
+}
